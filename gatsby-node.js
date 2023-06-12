e38d76aef596378cb864afc513b80f91d291c003
timoprice <91557541+timoprice@users.noreply.github.com>
2023-06-12 13:31:26 -0500

    Update gatsby-node.js

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,3 +1,39 @@
+const path = require(`path`)
+// Log out information after a build is done
+exports.onPostBuild = ({ reporter }) => {
+  reporter.info(`Your Gatsby site has been built!`)
+}
+// Create blog pages dynamically
+exports.createPages = async ({ graphql, actions }) => {
+  const { createPage } = actions
+  const blogPostTemplate = path.resolve(`src/templates/blog-post.js`)
+  const result = await graphql(`
+    query {
+      allSamplePages {
+        edges {
+          node {
+            slug
+            title
+          }
+        }
+      }
+    }
+  `)
+  result.data.allSamplePages.edges.forEach(edge => {
+    createPage({
+      path: `${edge.node.slug}`,
+      component: blogPostTemplate,
+      context: {
+        title: edge.node.title,
+      },
+    })
+  })
+}
+
+/*
+
+
+
 const path = require(`path`);
 const { createFilePath } = require(`gatsby-source-filesystem`);
 
@@ -103,3 +139,5 @@ exports.createSchemaCustomization = ({ actions }) => {
   `;
   createTypes(typeDefs);
 };
+
+/*
